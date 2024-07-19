5e468b9834302b5677755f63b8e461b80d4a59e6
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2024-07-19 15:31:15 +0530

    commit 1

---
+++ day1.py
@@ -0,0 +1,72 @@
+name='John'
+print(name)
+#John
+
+
+name=input("Enter your name : ")
+#Enter your name : imon
+print("Hello! ", name)
+#Hello! imon
+print("Hello! "+ name)
+#Hello!imon
+
+
+a=input("enter a number")
+#enter a number5
+b=input("enter another number")
+#enter another number17
+c=a+b
+print(c)
+#517
+
+a=int(input("enter a number"))
+#enter a number5
+b=int(input("enter another number"))
+#enter another number17
+c=a+b
+print(c)
+
+
+#a=input("enter a number")
+#b=input("enter another number")
+#c=a-b#error
+#print(c)
+
+a=int(input("enter a number"))
+#enter a number5
+b=int(input("enter another number"))
+#enter another number17
+c=a-b
+print(c)
+
+a=int(input("enter a number"))
+#enter a number5
+b=int(input("enter another number"))
+#enter another number17
+c=a*b
+print(c)
+
+a=int(input("enter a number"))
+#enter a number5
+b=int(input("enter another number"))
+#enter another number17
+c=a/b
+print(c)
+print(a%b)
+
+a=int(input("enter a number"))
+#enter a number5
+b=int(input("enter another number"))
+#enter another number17
+c=a//b
+print(c)
+
+a=int(input("enter a number"))
+#enter a number5
+b=int(input("enter another number"))
+#enter another number17
+c=a**b
+print(c)
+
+
+
---
+++ f1.py
@@ -0,0 +1,29 @@
+print("hello world")
+
+if True:
+    print("true")
+else:
+    print("false")
+
+
+word='word'
+sentence="This is a sentence."
+paragraph="""This is a sentence. It is
+made up of multiple lines  and sentence"""
+sentences='This is a sentence.'
+
+print(word)
+print(sentence)
+print(paragraph)
+print(sentences)
+
+a=b=c=1
+a2,b2,c2=1,2,"john"
+
+print(a)
+print(b)
+print(c)
+
+print(a2)
+print(b2)
+print(c2)
